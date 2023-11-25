ec29852410fede98b52f68ead3bbee6e13da5f05
João Egydio <egydiio@hotmail.com>
2023-11-25 16:53:21 -0300

    Criando Interface

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks.Dataflow;
 
 interface Conta{
     void CalcularConta();
@@ -23,6 +25,149 @@ public class Consumidor
     public TipoConsumidor Tipo { get; set; }
 }
 
+public class Tables
+{
+
+    public void DashboardTable()
+    {
+        string dashTable = "+----------------------------------------+\n";
+        dashTable += "|               Bem Vindo!!              |\n";
+        dashTable += "|----------------------------------------|\n";
+        dashTable += "| 1 - Cadastrar                          |\n";
+        dashTable += "| 2 - Login                              |\n";
+        dashTable += "| 0 - Sair                               |\n";
+        dashTable += "+----------------------------------------+";
+        Console.WriteLine(dashTable);
+        this.Quest();
+    }
+
+    public void LoginTable()
+    {
+        string loginTable = "+----------------------------------------+\n";
+        loginTable += "|                 Login                  |\n";
+        loginTable += "+----------------------------------------+";
+        Console.WriteLine(loginTable);
+    }
+
+    public void RegisterTable()
+    {
+        string registerTable = "+----------------------------------------+\n";
+        registerTable += "|                Cadastrar               |\n";
+        registerTable += "+----------------------------------------+";
+        Console.WriteLine(registerTable);
+    }
+
+    public void FileTable()
+    {
+        string fileTable = "+----------------------------------------+\n";
+        fileTable += "|           Adicionar Arquivo            |\n";
+        fileTable += "|----------------------------------------|\n";
+        fileTable += "| 1 - Você deseja adicionar um arquivo ? |\n";
+        fileTable += "| 0 - Sair                               |\n";
+        fileTable += "+----------------------------------------+";
+        Console.WriteLine(fileTable);
+    }
+
+    public void AddFileTable()
+    {
+        string addfileTable = "Mova o arquivo para pasta\n";
+        addfileTable += "Digite o nome do arquivo: ";
+        Console.Write(addfileTable);
+    }
+    public void ConsultTable()
+    {
+        string opcao;
+        do
+        {
+            string tablePrinc = "+---------------------------------------------------------+\n";
+            tablePrinc += "|                    Consulte sua conta                   |\n";
+            tablePrinc += "|---------------------------------------------------------|\n";
+            tablePrinc += "| 1 - Qual foi meu consumo de energia/água no último mês? |\n";
+            tablePrinc += "| 2 - Qual é o valor total da minha conta?                |\n";
+            tablePrinc += "| 3 - Qual é o valor da minha conta sem impostos?         |\n";
+            tablePrinc += "| 4 - Perguntas Adicionais                                |\n";
+            tablePrinc += "| 0 - Sair                                                |\n";
+            tablePrinc += "+---------------------------------------------------------+";
+            Console.WriteLine(tablePrinc);
+            this.Quest();
+            opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    // Lógica para consultar o consumo de energia/água no último mês
+                    break;
+
+                case "2":
+                    // Lógica para consultar o valor total da conta
+                    break;
+
+                case "3":
+                    // Lógica para consultar o valor total da conta
+                    break;
+
+                case "4":
+                    this.AdcTable();
+                    break;
+
+                case "0":
+                    // Sair da consulta
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        } while (opcao != "0");
+    }
+
+    public void AdcTable()
+    {
+        string opcao;
+        do
+        {
+            string tableAdc = "+------------------------------------------------------------------------------------+\n";
+            tableAdc += "|                                Perguntas Adicionais                                |\n";
+            tableAdc += "|------------------------------------------------------------------------------------|\n";
+            tableAdc += "| 1 - Quanto variou minha conta, em reais e em consumo, entre dois meses escolhidos? |\n";
+            tableAdc += "| 2 - Qual é o valor médio da minha conta de energia/água?                           |\n";
+            tableAdc += "| 3 - Em que mês houve a conta de maior valor, em reais e em consumo?                |\n";
+            tableAdc += "| 0 - Sair                                                                           |\n";
+            tableAdc += "+------------------------------------------------------------------------------------+";
+            Console.WriteLine(tableAdc);
+            this.Quest();
+            opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    // Lógica para consultar o consumo de energia/água no último mês
+                    break;
+
+                case "2":
+                    // Lógica para consultar o valor total da conta
+                    break;
+
+                case "3":
+                    // Lógica para consultar o valor total da conta
+                    break;
+
+                case "0":
+                    this.ConsultTable();
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        } while(opcao != "0");
+    }
+
+    public void Quest()
+    {
+        Console.Write("Escolha uma opção: ");
+    }
+}
 public class ContaEnergia : Conta
 {
     public Consumidor? Consumidor { get; set; }
@@ -236,13 +381,24 @@ public class ContaAgua : Conta
 
 class Program
 {
-    static void Main()
-    {
-        string caminhoArquivo = "Contas/Arquivo.txt";
 
+    static bool ProcessarContas(string caminhoArquivo = "Contas/ContaPrincipal/Arquivo.txt")
+    {
         try
         {
+            if (!File.Exists(caminhoArquivo))
+            {
+                Console.WriteLine($"Erro: O arquivo '{caminhoArquivo}' não foi encontrado.");
+                return false;
+            }
+
             string[] linhas = File.ReadAllLines(caminhoArquivo);
+
+            if (linhas.Length == 0)
+            {
+                Console.WriteLine($"Erro: O arquivo '{caminhoArquivo}' está vazio.");
+                return false;
+            }
 
             foreach (string linha in linhas)
             {
@@ -262,8 +418,7 @@ class Program
                 };
 
                 contaAgua.CalcularConta();
-
-                Console.WriteLine(contaAgua);
+                /* Console.WriteLine(contaAgua); */
 
                 ContaEnergia contaEnergia = new ContaEnergia
                 {
@@ -273,15 +428,122 @@ class Program
                 };
 
                 contaEnergia.CalcularConta();
-
-                Console.WriteLine(contaEnergia);
+                /* Console.WriteLine(contaEnergia); */
 
                 Console.WriteLine();
             }
+            return true; // Retorna true se o processamento for bem-sucedido
         }
         catch (IOException e)
         {
             Console.WriteLine($"Ocorreu um erro ao ler o arquivo: {e.Message}");
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine($"Erro: Sem permissão para acessar o arquivo '{caminhoArquivo}'.");
+        }
+        return false; // Retorna false em caso de erro
+    }
+
+    static void Dashboard()
+    {
+        Tables tables = new Tables();
+        int login;
+
+        // Exibir a tabela de login
+        tables.Quest();
+        login = int.Parse(Console.ReadLine());
+
+        // Verificar se a pessoa quer adicionar um arquivo
+        tables.FileTable();
+        tables.Quest();
+        int opcaoAdicionarArquivo = int.Parse(Console.ReadLine());
+
+        if (opcaoAdicionarArquivo == 1)
+        {
+            // Exibir a tabela de adicionar arquivo
+            string nomeArq;
+            string caminhoCompleto;
+            do
+            {
+                Console.WriteLine();
+                tables.AddFileTable();
+                nomeArq = Console.ReadLine();
+                caminhoCompleto = Path.Combine("Contas", nomeArq);
+            } while (ProcessarContas(caminhoCompleto) == false);
+            
+            Console.WriteLine(caminhoCompleto);
+            if (ProcessarContas(caminhoCompleto))
+            {
+                tables.ConsultTable();
+            }
+        }
+        else
+        {
+            tables.ConsultTable();
+        } 
+    }
+    static void Main()
+    {
+        
+        Tables tables = new Tables();
+        tables.DashboardTable();
+        string opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+            case "1":
+                    tables.RegisterTable();
+                    Console.Write("Escreva seu nome: ");
+                    string nome = Console.ReadLine();
+                    break;
+            case "2":
+                    tables.LoginTable();
+                    Console.Write("Escreva seu ID: ");
+                    string ID = Console.ReadLine();
+                    break;
+            case "0":
+                    break;
+            default:
+                Console.WriteLine("Opção inválida.");
+                    break;
+        }
+        
+
+
+        int login;
+
+        // Exibir a tabela de login
+        tables.Quest();
+        login = int.Parse(Console.ReadLine());
+
+        // Verificar se a pessoa quer adicionar um arquivo
+        tables.FileTable();
+        tables.Quest();
+        int opcaoAdicionarArquivo = int.Parse(Console.ReadLine());
+
+        if (opcaoAdicionarArquivo == 1)
+        {
+            // Exibir a tabela de adicionar arquivo
+            string nomeArq;
+            string caminhoCompleto;
+            do
+            {
+                Console.WriteLine();
+                tables.AddFileTable();
+                nomeArq = Console.ReadLine();
+                caminhoCompleto = Path.Combine("Contas", nomeArq);
+            } while (ProcessarContas(caminhoCompleto) == false);
+            
+            Console.WriteLine(caminhoCompleto);
+            if (ProcessarContas(caminhoCompleto))
+            {
+                tables.ConsultTable();
+            }
+        }
+        else
+        {
+            tables.ConsultTable();
         }
     }
 }
