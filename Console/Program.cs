3025330d8468c175684a03cf4fefd6445165001b
João Egydio <egydiio@hotmail.com>
2023-11-26 17:04:36 -0300

    arrumando interface

--- Console/Program.cs
+++ Console/Program.cs
@@ -8,8 +8,8 @@ using System.Threading.Tasks.Dataflow;
 class Program
 {
     public static int id = 0;
-    public static StreamWriter sw = File.AppendText("Consumidores.txt");
-    static bool ProcessarContas(string caminhoArquivo = "Contas/ContaPrincipal/Arquivo.txt")
+    public static StreamWriter sw = File.AppendText("Tabelas/Consumidores.txt");
+    static bool ProcessarContas(string caminhoArquivo = "Arquivos/ContaPrincipal/Arquivo.txt")
     {
         try
         {
@@ -75,11 +75,6 @@ class Program
     static void Dashboard()
     {
         Tables tables = new Tables();
-        int login;
-
-        // Exibir a tabela de login
-        tables.Quest();
-        login = int.Parse(Console.ReadLine());
 
         // Verificar se a pessoa quer adicionar um arquivo
         tables.FileTable();
@@ -111,10 +106,76 @@ class Program
         } 
     }
 
-    public static void escreveConsumidores(string nome){
-        sw.WriteLine(id + "," + nome); 
-        id++;
+    public static string escreveConsumidores(string nome)
+    {
+        try
+        {
+            int proximoID = ObterProximoID();
+
+            // Escrever no arquivo usando o próximo ID
+            sw.WriteLine(proximoID + "," + nome);
+
+            // Fechar o StreamWriter
+            sw.Close();
+
+            Console.WriteLine("Seu ID é: " + proximoID);
+
+            return "true";
+        }
+        catch (Exception ex)
+        {
+            // Certifique-se de fechar o StreamWriter em caso de exceção
+            sw.Close();
+
+            // Retorna a mensagem de erro
+            return "Erro ao escrever no arquivo: " + ex.Message;
+        }
     }
+
+    public static int ObterProximoID()
+    {
+        // Lê todas as linhas do arquivo para determinar o maior ID atual
+        int maiorID = File.ReadAllLines("Tabelas/Consumidores.txt")
+                        .Select(line => int.Parse(line.Split(',')[0]))
+                        .DefaultIfEmpty(0)
+                        .Max();
+
+        // Incrementa o maior ID para obter o próximo ID
+        return maiorID + 1;
+    }
+
+    public static bool VerificarExistenciaID(int id)
+    {
+        try
+        {
+            // Lê todas as linhas do arquivo
+            string[] linhas = File.ReadAllLines("Tabelas/Consumidores.txt");
+
+            // Verifica se o ID está presente em alguma linha
+            foreach (string linha in linhas)
+            {
+                string[] dados = linha.Split(',');
+
+                if (dados.Length > 0 && int.TryParse(dados[0], out int consumidorID))
+                {
+                    if (consumidorID == id)
+                    {
+                        // ID encontrado
+                        return true;
+                    }
+                }
+            }
+
+            // ID não encontrado
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao verificar existência do ID: " + ex.Message);
+            return false;
+        }
+    }
+
     static void Main()
     {
         bool run = true;
@@ -125,64 +186,55 @@ class Program
             tables.DashboardTable();
             string opcao = Console.ReadLine();
             switch (opcao){
-            case "1":
-                Console.Clear();
-                tables.RegisterTable();
-                Console.Write("Escreva seu nome: ");
-                string nome = Console.ReadLine();
-                escreveConsumidores(nome);
-                break;
-            case "2":
-                Console.Clear();
-                tables.LoginTable();
-                Console.Write("Escreva seu ID: ");
-                string ID = Console.ReadLine();
-                break;
-            case "0":
-                Console.Clear();
-                Console.WriteLine("Obrigado por usar nossa aplicação !!!");
-                run = false;
-                sw.Close();
-                break;
-            default:
-                Console.WriteLine("Opção inválida.");
-                break;
+                case "1":
+                    Console.Clear();
+                    tables.RegisterTable();
+                    Console.Write("Escreva seu nome: ");
+                    string nome = Console.ReadLine();
+                    string consu = escreveConsumidores(nome);
+                    if (consu == "true")
+                    {
+                        goto case "2";
+                    } else 
+                    {
+                        Console.WriteLine(consu);
+                    }
+                    break;
+                case "2":
+                    tables.LoginTable();
+                    Console.Write("Escreva seu ID: ");
+                    string idString = Console.ReadLine();
+                    if (int.TryParse(idString, out int id))
+                    {
+                        if (VerificarExistenciaID(id))
+                        {
+                            // ID existe, permitir login
+                            Console.WriteLine("Login bem-sucedido!");
+                            run = false;
+                        }
+                        else
+                        {
+                            // ID não existe, mostrar mensagem de erro
+                            Console.WriteLine("Erro: ID não encontrado. Tente novamente.");
+                        }
+                    }
+                    else
+                    {
+                        // Entrada inválida para ID, mostrar mensagem de erro
+                        Console.WriteLine("Erro: ID inválido. Tente novamente.");
+                    }
+                    break;
+                case "0":
+                    Console.Clear();
+                    Console.WriteLine("Obrigado por usar nossa aplicação !!!");
+                    run = false;
+                    sw.Close();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
         }
-        }
-
-        // int login;
-
-        // // Exibir a tabela de login
-        // tables.Quest();
-        // login = int.Parse(Console.ReadLine());
-
-        // // Verificar se a pessoa quer adicionar um arquivo
-        // tables.FileTable();
-        // tables.Quest();
-        // int opcaoAdicionarArquivo = int.Parse(Console.ReadLine());
-
-        // if (opcaoAdicionarArquivo == 1)
-        // {
-        //     // Exibir a tabela de adicionar arquivo
-        //     string nomeArq;
-        //     string caminhoCompleto;
-        //     do
-        //     {
-        //         Console.WriteLine();
-        //         tables.AddFileTable();
-        //         nomeArq = Console.ReadLine();
-        //         caminhoCompleto = Path.Combine("Contas", nomeArq);
-        //     } while (ProcessarContas(caminhoCompleto) == false);
-            
-        //     Console.WriteLine(caminhoCompleto);
-        //     if (ProcessarContas(caminhoCompleto))
-        //     {
-        //         tables.ConsultTable();
-        //     }
-        // }
-        // else
-        // {
-        //     tables.ConsultTable();
-        // }
+        Dashboard();
     }
 }
